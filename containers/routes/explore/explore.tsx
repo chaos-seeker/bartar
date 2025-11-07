cec63ce4335266f46cb6e9957f9874fd34e4feca
hamid shahsavani <051.sys113@gmail.com>
2025-11-07 14:29:09 +0330

    chore: new changes

--- containers/routes/explore/explore.tsx
+++ containers/routes/explore/explore.tsx
@@ -12,7 +12,7 @@ export async function Explore() {
             explore products ({products.length})
           </p>
         </div>
-        <div className="container py-4">
+        <div className="container">
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
             {products.map((p) => (
               <div key={p._id}>
